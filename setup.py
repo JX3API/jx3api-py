3ada7ba2ab8537d82a8a25d8802c34882d75abe8
Oi <112309223+OmegaIsMe@users.noreply.github.com>
2024-04-17 15:15:06 +0800

    docs(setup): add long_description for setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="jx3api",
     version="20240416",
     description="The Python SDK to the JX3API",
+    long_description="The Python SDK to the JX3API",
     author="JX3API",
     url="https://www.jx3api.com",
     packages=find_packages(),
